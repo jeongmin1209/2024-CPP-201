ea85b42602c3a123dcbed7a41a7786a80c6fae2f
simjeongmin <s2327@e-mirim.hs.kr>
2024-04-08 14:45:14 +0900

    클래스는 디폴트가 private

---
+++ 2024-CPP-201/02-05 문자열 비교 - 복사본.cpp
@@ -0,0 +1,9 @@
+﻿#include <string>
+#include <iostream>
+using namespace std;
+
+void main(void) {
+	string str = "Juyoung";
+	cout << str.size() << endl;
+	cout << str.length() << endl;
+}
--- 2024-CPP-201/02-05 문자열 비교.cpp
+++ 2024-CPP-201/02-05 문자열 비교.cpp
@@ -2,10 +2,14 @@
 #include <iostream>
 using namespace std;
 
+// 클래스는 디폴트가 private
+struct Student {
+	int hakbun_;
+	string name_;
+};
 void main(void) {
-	string str = "Juyoung";
-	if (str == "Juyoung")
-		cout << "같다";
-	else 
-		cout << "다르다";
+	struct Student juyoung;
+	juyoung.hakbun_ = 2115;
+	juyoung.name_ = "윤주영";
+
 }
