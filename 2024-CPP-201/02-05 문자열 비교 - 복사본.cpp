4f497a804294e8054bbfd155ceb3ca436ccf3e5c
simjeongmin <s2327@e-mirim.hs.kr>
2024-04-08 15:01:47 +0900

    생성자

--- 2024-CPP-201/02-05 문자열 비교 - 복사본.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include <string>
-#include <iostream>
-using namespace std;
-
-void main(void) {
-	string str = "Juyoung";
-	cout << str.size() << endl;
-	cout << str.length() << endl;
-}
---
+++ 2024-CPP-201/03-01 구조체와 클래스.cpp
@@ -0,0 +1,20 @@
+﻿#include <string>
+#include <iostream>
+using namespace std;
+
+class Student {
+public:
+	Student(int hakbun, string name) {
+		hakbun = hakbun;
+		name_ = name;
+	}
+private:
+	int hakbun_;
+	string name_;
+};
+
+void main(void) {
+	// 매개 변수가 없는 생성자를 호출
+	// Student juyoung = Student();
+	Student juyoung = Student(2115, "윤주영");
+}
