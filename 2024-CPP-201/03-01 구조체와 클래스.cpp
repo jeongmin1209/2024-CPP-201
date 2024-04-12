a92994f461467234113e2671c1268595c1aa8a2e
simjeongmin <s2327@e-mirim.hs.kr>
2024-04-12 10:13:12 +0900

    03-01 구조체와 배열

--- 2024-CPP-201/03-01 구조체와 클래스.cpp
+++ 2024-CPP-201/03-01 구조체와 클래스.cpp
@@ -1,5 +1,6 @@
 ﻿#include <string>
 #include <iostream>
+#include <print>
 using namespace std;
 
 class Student {
@@ -8,13 +9,27 @@ public:
 		hakbun = hakbun;
 		name_ = name;
 	}
+	Student() {
+		hakbun_ = 2121;
+		name_ = "JWP"
+	}
+	
+	void 
 private:
 	int hakbun_;
 	string name_;
 };
 
 void main(void) {
-	// 매개 변수가 없는 생성자를 호출
-	// Student juyoung = Student();
-	Student juyoung = Student(2115, "윤주영");
+	Student* stu = new Student[3]{
+		{2115, "윤주영"},
+		{2121, "JWP"},
+		{2104, "위즈덤"}
+	};
+
+	for (int i = 0; i < 3; i++) {
+		stu[i].print();
+		// 동적 할당된 배열 삭제
+		delete[]stu;
+	}
 }
